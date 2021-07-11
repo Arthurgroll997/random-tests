58429e23293b1b3ea6ae5241108cb014552a90b4
Arthurgroll123 <apvgroll@gmail.com>
2021-07-11 04:36:06 -0300

    [+] Added pattern scanner

--- GettingInformation/GettingInformation/main.c
+++ GettingInformation/GettingInformation/main.c
@@ -17,7 +17,13 @@ int main()
             return 0;
         }
 
-        printf("A base de %s e: 0x%08X", targetProcess, getModuleBaseAddrExternal(hProc, targetProcess));
+        DWORD moduleBaseAddr = getModuleBaseAddrExternal(hProc, targetProcess);
+        DWORD moduleSize = getModuleSizeExternal(hProc, targetProcess);
+        DWORD patternAddr = (DWORD)(findPattern(hProc, moduleBaseAddr, moduleSize + 1, "\x8B\x44\x24\x2C\x89\x8A\xFC\x00\x00\x00\x89\x82\xF8\x00\x00\x00", "xxxxxxxxxxxxxxxx", 16)) + 10;
+
+        printf("A base de %s e: 0x%08X\n", targetProcess, moduleBaseAddr);
+        printf("O tamanho de %s e: 0x%08X\n", targetProcess, moduleSize);
+        printf("O endereco do padrao escolhido e: 0x%08X\n", patternAddr);
 
         CloseHandle(hProc);
     }
@@ -28,6 +34,47 @@ int main()
     return 0;
 }
 
+LPVOID findPattern(HANDLE hProc, DWORD startAddress, DWORD maxSizeToSearch, char* bytes, char* pattern, DWORD size)
+{
+    DWORD realSize = size ? size : strlen(bytes);
+    BOOL patternFound = FALSE;
+    BYTE* bytesConverted = (BYTE*)bytes;
+
+    if (startAddress < 0x400000) return NULL; // We want at least the first module in the module list
+
+    BYTE currentByte = 0;
+    DWORD actualPosition = 0;
+    DWORD addressFound = 0;
+    DWORD protectionSize = 1;
+    DWORD oldProtection;
+    DWORD realAddress;
+
+    for (DWORD i = 0; (!patternFound) && (i < maxSizeToSearch); i++)
+    {
+        realAddress = startAddress + i;
+        if (actualPosition == realSize) patternFound = TRUE;
+
+        ntUtils.ntProtectMemory(hProc, &realAddress, &protectionSize, PAGE_READONLY | PAGE_EXECUTE_READWRITE | PAGE_READWRITE, &oldProtection);
+        if (NT_SUCCESS(ntUtils.ntReadMemory(hProc, (BYTE*)(realAddress), &currentByte, 1, NULL)))
+        {
+            if ((bytesConverted[actualPosition] == currentByte) || (pattern[actualPosition] == '?'))
+            {
+                if (actualPosition == 0) addressFound = i;
+                actualPosition++;
+            }
+            else if (actualPosition > 0)
+            {
+                actualPosition = 0;
+                i = addressFound;
+            }
+
+            ntUtils.ntProtectMemory(hProc, &realAddress, &protectionSize, oldProtection, NULL);
+        }
+    }
+
+    return patternFound ? realAddress - realSize : NULL;
+}
+
 PVOID getExternalInfo(HANDLE hProc, ULONG address, ULONG size)
 {
     PVOID baseAddr = calloc(size, sizeof(BYTE)); // I like to use it this way, but feel free to use malloc(size) if you want to.
@@ -35,6 +82,78 @@ PVOID getExternalInfo(HANDLE hProc, ULONG address, ULONG size)
     if (!NT_SUCCESS(ntUtils.ntReadMemory(hProc, address, baseAddr, size, NULL))) return NULL;
 
     return baseAddr;
+}
+
+DWORD getModuleSize(HANDLE hProc, char* moduleName)
+{
+    PWCHAR convertedModuleName = (PWCHAR)calloc(strlen(moduleName) + 1, sizeof(WCHAR));
+    mbstowcs(convertedModuleName, moduleName, strlen(moduleName));
+
+    PPEB procPeb = getProcessPEB(hProc);
+    PLIST_ENTRY moduleList = procPeb->LoaderData->InLoadOrderModuleList.Flink;
+    PLDR_MODULE firstModule = (PLDR_MODULE)CONTAINING_RECORD(moduleList, LDR_MODULE, InLoadOrderModuleList);
+
+    PLDR_MODULE nextModule = firstModule;
+    BOOL started = TRUE;
+
+    while (((DWORD)moduleList != (DWORD)firstModule) || (started))
+    {
+        nextModule = (PLDR_MODULE)CONTAINING_RECORD(moduleList, LDR_MODULE, InLoadOrderModuleList);
+
+        if (wcsstr(nextModule->FullDllName.Buffer, convertedModuleName))
+        {
+            return (DWORD)nextModule->SizeOfImage;
+        }
+
+        moduleList = moduleList->Flink;
+    }
+
+    return 0;
+}
+
+DWORD getModuleSizeExternal(HANDLE hProc, char* moduleName)
+{
+    PWCHAR convertedModuleName = (PWCHAR)calloc(strlen(moduleName) + 1, sizeof(WCHAR));
+    mbstowcs(convertedModuleName, moduleName, strlen(moduleName));
+
+    PPEB peb = getProcessPEB(hProc);
+    PPEB internalPeb = getProcessPEB(GetCurrentProcess());
+
+    PPEB_LDR_DATA pebLdrData = GET_EXT_INFO(hProc, GET_REAL_ADDRESS(peb, internalPeb, LoaderData), PPEB_LDR_DATA, sizeof(ULONG));  // We want the offset so that we can read the "real" LoaderData from the target process. After we get the offset, what we do is read 4 bytes from the address (that is offset + the peb addres), so we get the pointer to what we want.
+    PPEB_LDR_DATA internalPebLdrData = internalPeb->LoaderData;
+
+    LIST_ENTRY moduleList = GET_EXT_INFO(hProc, GET_REAL_ADDRESS(pebLdrData, internalPebLdrData, InLoadOrderModuleList), LIST_ENTRY, sizeof(LIST_ENTRY));
+    PLIST_ENTRY realModuleList = GET_REAL_ADDRESS(pebLdrData, internalPebLdrData, InLoadOrderModuleList);
+    PLIST_ENTRY internalModuleList = internalPeb->LoaderData->InLoadOrderModuleList.Flink;
+
+    PLDR_MODULE firstModule = GET_EXT_INFO(hProc, (DWORD)CONTAINING_RECORD(realModuleList, LDR_MODULE, InLoadOrderModuleList), PLDR_MODULE, sizeof(ULONG));
+    PLDR_MODULE internalFirstModule = (PLDR_MODULE)CONTAINING_RECORD(internalModuleList, LDR_MODULE, InLoadOrderModuleList);
+
+    DWORD firstModDword = (DWORD)firstModule;
+    PLDR_MODULE currentModule = firstModule;
+    PLDR_MODULE internalCurrentModule = internalFirstModule;
+
+    do
+    {
+        UNICODE_STRING name = GET_EXT_INFO(hProc, GET_REAL_ADDRESS(currentModule, internalCurrentModule, FullDllName), UNICODE_STRING, sizeof(UNICODE_STRING));
+        UNICODE_STRING internalName = internalCurrentModule->FullDllName;
+        name.Buffer = getExternalInfo(hProc, name.Buffer, name.Length + 2);
+
+        if (name.Buffer == NULL) return 0;
+
+        if (wcsstr(name.Buffer, convertedModuleName))
+        {
+            return (DWORD)GET_EXT_INFO(hProc, GET_REAL_ADDRESS(currentModule, internalCurrentModule, SizeOfImage), PVOID, sizeof(ULONG));
+        }
+
+        realModuleList = GET_EXT_INFO(hProc, GET_REAL_ADDRESS(realModuleList, internalModuleList, Flink), PLIST_ENTRY, sizeof(ULONG));
+        internalModuleList = internalModuleList->Flink;
+
+        currentModule = GET_EXT_INFO(hProc, (DWORD)CONTAINING_RECORD(realModuleList, LDR_MODULE, InLoadOrderModuleList), PLDR_MODULE, sizeof(ULONG));
+
+    } while (TRUE);
+
+    return 0;
 }
 
 DWORD getModuleBaseAddr(HANDLE hProc, char* moduleName)
@@ -140,19 +259,14 @@ DWORD getProcId(char* procName)
 
     PWCHAR procNameConverted = (PWCHAR)calloc(strlen(procName) + 1, sizeof(WCHAR));
     mbstowcs(procNameConverted, procName, strlen(procName));
-    DWORD firstProc = NULL;
 
     for (; procInfo; procInfo = (PSYSTEM_PROCESS_INFORMATION)((DWORD)procInfo + (DWORD)procInfo->NextEntryOffset))
     {
-        if (firstProc == (DWORD)procInfo) return 0;
+        if (((DWORD)procInfo == NULL) || (procInfo->NextEntryOffset == 0)) return 0;
 
         if (procInfo->ImageName.Length == 0)
         {
             continue;
-        }
-        else if (firstProc == NULL)
-        {
-            firstProc = (DWORD)procInfo;
         }
 
         if (!wcscmp(procInfo->ImageName.Buffer, procNameConverted))
--- GettingInformation/GettingInformation/main.h
+++ GettingInformation/GettingInformation/main.h
@@ -36,9 +36,12 @@ char* targetProcess;
 HANDLE openProc(char* procName);
 BOOL isProcessX86(char* procName, PBOOL returnValue); // Actually, I'll return whether the function worked or not and the actual return value (if the process is x86) will be set in the returnValue parameter.
 DWORD getModuleBaseAddr(HANDLE hProc, char* moduleName); // Has to be used internally.
+DWORD getModuleSize(HANDLE hProc, char* moduleName); // Has to be used internally.
+DWORD getModuleSizeExternal(HANDLE hProc, char* moduleName); // Used externally (or internally if GetCurrentProcess() is passed in hProc).
 DWORD getModuleBaseAddrExternal(HANDLE hProc, char* moduleName); // I can't remember the last time I messed up so hard with PEB, this experience was awesome! (Btw, I've never accessed the PEB of processes outside of kernel-mode, so this was indeed a pain in the ass [as I'm dumb enough to don't google examples and try to discover things by myself], but it was good anyways)
 PPEB getProcessPEB(HANDLE hProc);
 PVOID getExternalInfo(HANDLE hProc, ULONG address, ULONG size); // I'll use it whenever I have to get, for example, the loader data of another process, since I'll get the address relative to the target process. The return value will be the address where the information is stored.
 DWORD getProcId(char* procName);
-int main();
 BOOL initializeNTFunctions();
+LPVOID findPattern(HANDLE hProc, DWORD startAddress, DWORD maxSizeToSearch, char* bytes, char* pattern, DWORD size); // We will use "x" when the byte is necessary and imutable, and "?" when the byte is useless. The address returned will be the address of the first byte in the pattern, even if it is useless (marked with a "?").
+int main();
